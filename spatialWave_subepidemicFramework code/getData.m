b52a078bf4245eb87a3f0ac63d514427ce8e41bf
gchowell <31742415+gchowell@users.noreply.github.com>
2022-11-21 18:19:46 -0500

    Add files via upload

--- spatialWave_subepidemicFramework code/getData.m
+++ spatialWave_subepidemicFramework code/getData.m
@@ -1,15 +1,17 @@
-function data2=getData(cadtemporal,datevecfirst1,datevecend1,date1,outbreak1,forecastingperiod)
+function data2=getData(cadtemporal,caddisease,datatype,cadregion,DT,datevecfirst1,datevecend1,date1,outbreak1,forecastingperiod)
 
-filename1=strcat('./input/cumulative-',cadtemporal,'-coronavirus-cases-USA-',datestr(datenum(datevecend1),'mm-dd-yy'),'.txt');
+filename1=strcat('./input/cumulative-',cadtemporal,'-',caddisease,'-',datatype,'-',cadregion,'-',datestr(datenum(datevecend1),'mm-dd-yy'),'.txt');
 
 data=load(filename1);
+
 dataprov=data';
 
 data1=dataprov(outbreak1,:)';
 
 data1=[data1(1);diff(data1)];
 
-datenum1=datenum(date1)-datenum(datevecfirst1)+1
+datenum1=datenum(datevecfirst1):DT:datenum(date1);
 
-data2=data1(datenum1:datenum1+forecastingperiod-1);
+
+data2=data1(length(datenum1):1:length(datenum1)+forecastingperiod-1);
 
