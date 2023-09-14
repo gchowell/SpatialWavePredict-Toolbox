67ff2b4f803dc251c26d61a3e6902b2b9c09e240
Gerardo Chowell <31742415+gchowell@users.noreply.github.com>
2023-09-14 09:40:57 -0400

    Add files via upload

--- spatialWave_subepidemicFramework code/plotForecast_SW_subepidemicFramework.m
+++ spatialWave_subepidemicFramework code/plotForecast_SW_subepidemicFramework.m
@@ -522,14 +522,24 @@ for run_id=-1
 
                 set(gca, 'XTick', 0:3:length(dates1(:,1))-1);
                 set(gca, 'XTickLabel', strcat('\fontsize{14}',dates1(1:3:end,:)));
-            else
+
+                xticklabel_rotate;
+
+            elseif DT==7
 
                 set(gca, 'XTick', 0:2:length(dates1(:,1))-1);
                 set(gca, 'XTickLabel', strcat('\fontsize{14}',dates1(1:2:end,:)));
 
-            end
+                xticklabel_rotate;
 
-            xticklabel_rotate;
+            elseif DT==365
+
+                years1=wave(1)+timelags:wave(1)+timelags+length(dates1(:,1))-1;
+
+                set(gca,'XTick',0:1:length(years1)-1);
+                set(gca, 'XTickLabel', strcat('\fontsize{14}',num2str(years1')));
+
+            end
 
             line1=plot(line2(:,1),line2(:,2),'k--')
             set(line1,'LineWidth',2)
